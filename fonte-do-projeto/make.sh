3b11e1c98735e777c5c7e2facb1b58de9b31a896
Henrique AD <henriquead7@gmail.com>
2020-07-08 18:50:28 -0300

    Adicionando o fonte do projeto

---
+++ fonte-do-projeto/make.sh
@@ -0,0 +1,104 @@
+#!/bin/sh
+#xcursorgen based_arrow_down.in /home/henriquead/AD/HenriqueAD/cursors/based_arrow_down
+#xcursorgen based_arrow_up.in /home/henriquead/AD/HenriqueAD/cursors/based_arrow_up
+#xcursorgen center_ptr.in /home/henriquead/AD/HenriqueAD/cursors/center_ptr
+#xcursorgen hand1.in /home/henriquead/AD/HenriqueAD/cursors/hand1
+xcursorgen all-scroll.in /home/henriquead/AD/HenriqueAD/cursors/all-scroll
+xcursorgen bd_double_arrow.in /home/henriquead/AD/HenriqueAD/cursors/bd_double_arrow
+xcursorgen bottom_left_corner.in /home/henriquead/AD/HenriqueAD/cursors/bottom_left_corner
+xcursorgen bottom_right_corner.in /home/henriquead/AD/HenriqueAD/cursors/bottom_right_corner
+xcursorgen bottom_side.in /home/henriquead/AD/HenriqueAD/cursors/bottom_side
+xcursorgen bottom_tee.in /home/henriquead/AD/HenriqueAD/cursors/bottom_tee
+xcursorgen circle.in /home/henriquead/AD/HenriqueAD/cursors/circle
+xcursorgen copy.in /home/henriquead/AD/HenriqueAD/cursors/copy
+xcursorgen cross.in /home/henriquead/AD/HenriqueAD/cursors/cross
+xcursorgen crossed_circle.in /home/henriquead/AD/HenriqueAD/cursors/crossed_circle
+xcursorgen crosshair.in /home/henriquead/AD/HenriqueAD/cursors/crosshair
+xcursorgen dnd-ask.in /home/henriquead/AD/HenriqueAD/cursors/dnd-ask
+xcursorgen dnd-copy.in /home/henriquead/AD/HenriqueAD/cursors/dnd-copy
+xcursorgen dnd-link.in /home/henriquead/AD/HenriqueAD/cursors/dnd-link
+xcursorgen dnd-move.in /home/henriquead/AD/HenriqueAD/cursors/dnd-move
+xcursorgen dnd-none.in /home/henriquead/AD/HenriqueAD/cursors/dnd-none
+xcursorgen dotbox.in /home/henriquead/AD/HenriqueAD/cursors/dotbox
+xcursorgen fd_double_arrow.in /home/henriquead/AD/HenriqueAD/cursors/fd_double_arrow
+xcursorgen grabbing.in /home/henriquead/AD/HenriqueAD/cursors/grabbing
+xcursorgen hand2.in /home/henriquead/AD/HenriqueAD/cursors/hand2
+xcursorgen left_ptr.in /home/henriquead/AD/HenriqueAD/cursors/left_ptr
+xcursorgen left_ptr_watch.in /home/henriquead/AD/HenriqueAD/cursors/left_ptr_watch
+xcursorgen left_side.in /home/henriquead/AD/HenriqueAD/cursors/left_side
+xcursorgen left_tee.in /home/henriquead/AD/HenriqueAD/cursors/left_tee
+xcursorgen link.in /home/henriquead/AD/HenriqueAD/cursors/link
+xcursorgen ll_angle.in /home/henriquead/AD/HenriqueAD/cursors/ll_angle
+xcursorgen lr_angle.in /home/henriquead/AD/HenriqueAD/cursors/lr_angle
+xcursorgen move.in /home/henriquead/AD/HenriqueAD/cursors/move
+xcursorgen pencil.in /home/henriquead/AD/HenriqueAD/cursors/pencil
+xcursorgen plus.in /home/henriquead/AD/HenriqueAD/cursors/plus
+xcursorgen pointer-move.in /home/henriquead/AD/HenriqueAD/cursors/pointer-move
+xcursorgen question_arrow.in /home/henriquead/AD/HenriqueAD/cursors/question_arrow
+xcursorgen right_ptr.in /home/henriquead/AD/HenriqueAD/cursors/right_ptr
+xcursorgen right_side.in /home/henriquead/AD/HenriqueAD/cursors/right_side
+xcursorgen right_tee.in /home/henriquead/AD/HenriqueAD/cursors/right_tee
+xcursorgen sb_down_arrow.in /home/henriquead/AD/HenriqueAD/cursors/sb_down_arrow
+xcursorgen sb_h_double_arrow.in /home/henriquead/AD/HenriqueAD/cursors/sb_h_double_arrow
+xcursorgen sb_left_arrow.in /home/henriquead/AD/HenriqueAD/cursors/sb_left_arrow
+xcursorgen sb_right_arrow.in /home/henriquead/AD/HenriqueAD/cursors/sb_right_arrow
+xcursorgen sb_up_arrow.in /home/henriquead/AD/HenriqueAD/cursors/sb_up_arrow
+xcursorgen sb_v_double_arrow.in /home/henriquead/AD/HenriqueAD/cursors/sb_v_double_arrow
+xcursorgen tcross.in /home/henriquead/AD/HenriqueAD/cursors/tcross
+xcursorgen top_left_corner.in /home/henriquead/AD/HenriqueAD/cursors/top_left_corner
+xcursorgen top_right_corner.in /home/henriquead/AD/HenriqueAD/cursors/top_right_corner
+xcursorgen top_side.in /home/henriquead/AD/HenriqueAD/cursors/top_side
+xcursorgen top_tee.in /home/henriquead/AD/HenriqueAD/cursors/top_tee
+xcursorgen ul_angle.in /home/henriquead/AD/HenriqueAD/cursors/ul_angle
+xcursorgen ur_angle.in /home/henriquead/AD/HenriqueAD/cursors/ur_angle
+xcursorgen vertical-text.in /home/henriquead/AD/HenriqueAD/cursors/vertical-text
+xcursorgen watch.in /home/henriquead/AD/HenriqueAD/cursors/watch
+xcursorgen X_cursor.in /home/henriquead/AD/HenriqueAD/cursors/X_cursor
+xcursorgen xterm.in /home/henriquead/AD/HenriqueAD/cursors/xterm
+xcursorgen zoom-in.in /home/henriquead/AD/HenriqueAD/cursors/zoom-in
+xcursorgen zoom-out.in /home/henriquead/AD/HenriqueAD/cursors/zoom-out
+
+cd /home/henriquead/AD/HenriqueAD/cursors
+ln -sf    dotbox			draped_box
+ln -sf    dotbox			icon
+ln -sf    dotbox			target
+ln -sf    dotbox			dot_box_mask
+ln -sf    X_cursor			pirate
+ln -sf    left_ptr_watch	08e8e1c95fe2fc01f976f1e063a24ccd
+ln -sf    left_ptr_watch	3ecb610c1bf2410f44200f48c40d3599
+ln -sf    left_ptr			arrow
+ln -sf    left_ptr			top_left_arrow
+ln -sf    right_ptr			draft_large
+ln -sf    right_ptr			draft_small
+ln -sf    move				4498f0e0c1937ffe01fd06f973665830
+ln -sf    move				9081237383d90e509aa00f00170e968f
+ln -sf    copy				1081e37283d90000800003c07f3ef6bf
+ln -sf    copy				6407b0e94181790501fd1e167b474872
+ln -sf    cross				cross_reverse
+ln -sf    cross				diamond_cross
+ln -sf    hand2				9d800788f1b08800ae810202380a0822
+ln -sf    hand2				e29285e634086352946a0e7090d73106
+ln -sf    hand2				hand
+ln -sf    hand2				hand1
+ln -sf    grabbing			fleur
+ln -sf    question_arrow	d9ce0ab605698f320427677b458ad60b
+ln -sf    question_arrow	5c6cd98b3f3ebcb1f9c7f1c204630408
+ln -sf    question_arrow	help
+ln -sf    question_arrow	left_ptr_help
+ln -sf    link				3085a0e285430894940527032f8b26df
+ln -sf    link				640fb0e74195791501fd1ed57b41487f
+ln -sf    crossed_circle	03b6e0fcb3499374a867c041f52298f0
+ln -sf    fd_double_arrow	fcf1c3c7cd4491d801f1e1c78f100000
+ln -sf    bd_double_arrow	c7088f0f3e6c8088236ef8e1e3e70000
+#ln -sf    based_arrow_up	base_arrow_up
+#ln -sf    based_arrow_down	base_arrow_down
+ln -sf    sb_h_double_arrow h_double_arrow
+ln -sf    sb_h_double_arrow	14fef782d02440884392942c11205230
+ln -sf    h_double_arrow	028006030e0e7ebffc7f7070c0600140
+ln -sf    sb_v_double_arrow double_arrow
+ln -sf    sb_v_double_arrow v_double_arrow
+ln -sf    sb_v_double_arrow	2870a09082c103050810ffdffffe0204
+ln -sf    v_double_arrow	00008160000006810000408080010102
+#ln -sf    center_ptr		centre_ptr
+
+#cp -RPv * cd /home/henriquead/AD/HenriqueAD/cursors/
